8e321847ce456235a513f39765cf38f21d9610d3
Faricha Aulia <149360908+frchaulia@users.noreply.github.com>
2024-06-19 21:16:23 +0700

    profile new

---
+++ src/app/components/atoms/icon.tsx
@@ -0,0 +1,25 @@
+import { SVGProps } from "react";
+
+interface IconProps extends SVGProps<SVGSVGElement> {
+  size?: number;
+}
+
+const Icon: React.FC<IconProps> = ({ size = 24,...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className={`h-${size} w-${size}`}
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    {...props}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 5 5 0 014 0z"
+    />
+  </svg>
+);
+
+export default Icon;
